051c4b0374cbb5777db41c77dec3ecce3587ddcf
Louis Allard <la.deweb@yahoo.com>
2023-11-02 15:50:20 +0100

    Update.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/RepairCafe-Courchelettes" element={<Homepage />} />
-          <Route path="/apropos" element={<About />} />
-          <Route path="/membres" element={<Membres />} />
+          <Route exact path="/" component={<Homepage />} />
+          <Route  exact path="/apropos" component={<About />} />
+          <Route exact path="/membres" component={<Membres />} />
         </Routes>
     </Router>
     </div>
