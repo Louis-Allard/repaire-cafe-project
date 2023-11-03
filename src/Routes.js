6c1b504de73732efb9a4a1df58f78893b12e0238
Louis Allard <la.deweb@yahoo.com>
2023-11-03 10:00:48 +0100

    Minor fix.

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import About from "./pages/apropos";
-
 import Membres from "./pages/membres";
 
 function LaRoute() {
@@ -12,7 +11,6 @@ function LaRoute() {
       <Routes>
         <Route path="/RepairCafe-Courchelettes" element={<Homepage />} />
         <Route path="/apropos" element={<About />} />
-
         <Route path="/membres" element={<Membres />} />
       </Routes>
     </Router>
